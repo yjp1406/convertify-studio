34528cfa372a27d6f47248c415bcae6e189eb949
Yagnesh Patel <yagneshjp1406@gmail.com>
2025-11-19 12:31:16 +0530

    robot.txt added and some fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -42,8 +42,8 @@ const Layout = ({ children, title, description, keywords, showSidebar = false }:
                 </aside>
               )} */}
             </div>
-              <Footer />
           </div>
+              <Footer />
         </main>
       </div>
     </>
